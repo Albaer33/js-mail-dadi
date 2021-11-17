9b8f2d1ee3b15ec8fe1c3f30faae4a1461ff953f
Albaer33 <alisi3396@gmail.com>
2021-11-17 14:00:37 +0100

    mail finito

---
+++ mail/js/script.js
@@ -0,0 +1,17 @@
+// trova se l'email dell' utente è nella lista email autorizzate
+const email=['abc1@gmail.com','abc2@gmail.com','abc3@gmail.com','abc4@gmail.com','abc5@gmail.com','abc6@gmail.com','abc7@gmail.com','abc8@gmail.com','abc9@gmail.com','abcd@gmail.com',];
+const userEmail=prompt('immetti il tuo indirizzo email: ');
+let flag=false;
+
+for (let i=0; i<email.length; i++) {
+    thisEmail=email[i];
+    if (userEmail===thisEmail) {
+        flag=true
+    }
+}
+if (flag) {
+    alert('l\'indirizzo email è autorizzato ad accedere')
+}
+else {
+    alert('l\'indirizzo email NON è autorizzato ad accedere')
+}
